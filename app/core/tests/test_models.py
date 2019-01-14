0b4ccca64e023f1765937a25b68ed53ee5c0951a
ExcelE <eespina002@citymail.cuny.edu>
2019-01-13 20:11:08 -0500

    Tests for superuser creation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,3 +21,21 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        # Anything below `with` should raise value error
+        # if there's no ValueError thrown, then the
+        # test failed
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'password')
+
+    def test_create_new_superuser(self):
+        """Creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
